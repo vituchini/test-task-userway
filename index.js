18cf264367a6e9eb259bac5ba6ef537cd2f942bd
Alex <alex.progger@gmail.com>
2023-08-25 01:36:10 +0300

    Create function to update all alts for random words

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log(`Hello World`);
+(() => {
+    const RANDOM_WORD_API_URL = 'https://random-word-api.herokuapp.com/word?number=1'
+
+    const updateImage = async (img) => {
+        let word =`${Math.floor(Math.random() * 1000) + 1}`
+        try {
+            const response = await fetch(RANDOM_WORD_API_URL)
+            word = await response.json()
+        } catch (e) {
+            console.error('Error', e)
+        }
+        img.setAttribute('alt', word)
+    }
+
+    let images = [...document.querySelectorAll('img')]
+
+    images.forEach(img => {
+        updateImage(img)
+    })
+})()
